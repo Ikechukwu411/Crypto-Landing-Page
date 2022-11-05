7283385519156bb9e6aac02ad1890f6a87209d38
ike <contact.70mag@gmail.com>
2022-11-05 09:04:57 +0000

    New Repo

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const show = document.querySelector("#Show");
 const overlay = document.querySelector(".overlay");
 const form = document.querySelector(".form");
 const removeModal = document.getElementById("removeModal");
+const Money = document.querySelectorAll(".Money");
 
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
@@ -62,53 +63,73 @@ fetch("https://rest.coinapi.io/v1/trades/latest", obj)
       <p id="fetchCoin">Hot Coins</p>
       <div class="ApiFetch">
         <div>
-          <p id="AnotherColor">${arr[0].symbol_id}</p>
+          <p id="AnotherColor"><img src="./img/products-5-2022101518.jpg" alt="" width="5%"> ${arr[0].symbol_id}</p>
           <p>${arr[0].price}</p>
         </div>
         <div>
           <p>${arr[0].taker_side}</p>
           <p>${arr[0].size}</p>
         </div>
+        <div class="Apifetch-btn">
+        <button class="Money" href="https://google.com" type="button">Buy</button>
+        <button>Sell</button>
+      </div>
       </div>
       <div class="ApiFetch">
         <div>
-          <p id="AnotherColor">${arr[1].symbol_id}</p>
+          <p id="AnotherColor"><img src="./img/products-5-2022101518.jpg" alt="" width="5%"> ${arr[1].symbol_id}</p>
           <p>${arr[1].price}</p>
         </div>
         <div>
           <p>${arr[1].taker_side}</p>
           <p>${arr[1].size}</p>
         </div>
+        <div class="Apifetch-btn">
+        <button class="Money">Buy</button>
+        <button>Sell</button>
+      </div>
       </div>
       <div class="ApiFetch">
         <div>
-          <p id="AnotherColor">${arr[2].symbol_id}</p>
+          <p id="AnotherColor"><img src="./img/products-5-2022101518.jpg" alt="" width="5%"> ${arr[2].symbol_id}</p>
           <p>${arr[2].price}</p>
         </div>
         <div>
           <p>${arr[2].taker_side}</p>
           <p>${arr[2].size}</p>
         </div>
+        <div class="Apifetch-btn">
+        <button class="Money">Buy</button>
+        <button>Sell</button>
+      </div>
       </div>
       <div class="ApiFetch">
         <div>
-          <p id="AnotherColor">${arr[3].symbol_id}</p>
+          <p id="AnotherColor"><img src="./img/products-5-2022101518.jpg" alt="" width="5%"> ${arr[3].symbol_id}</p>
           <p>${arr[3].price}</p>
         </div>
         <div>
           <p>${arr[3].taker_side}</p>
           <p>${arr[3].size}</p>
         </div>
+        <div class="Apifetch-btn">
+        <button class="Money">Buy</button>
+        <button>Sell</button>
+      </div>
       </div>
       <div class="ApiFetch">
         <div>
-          <p id="AnotherColor">${arr[4].symbol_id}</p>
+          <p id="AnotherColor"><img src="./img/products-5-2022101518.jpg" alt="" width="5%">${arr[4].symbol_id}</p>
           <p>${arr[4].price}</p>
         </div>
         <div>
           <p>${arr[4].taker_side}</p>
           <p>${arr[4].size}</p>
         </div>
+        <div class="Apifetch-btn">
+        <button class="Money">Buy</button>
+        <button>Sell</button>
+      </div>
       </div>
     </div>`;
     });
